bcdc2797050be8bbca264d26148c29f3c8a214a0
Daryll Ko <daryll.ko@gmail.com>
2024-04-29 20:51:47 +0800

    update: tweak layout a bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -82,40 +82,38 @@
 			<h2 class="text-xl text-yellow-500">Your smart medicine box.</h2>
 		</div>
 
-		<div class="flex items-center gap-5">
-			<div class="flex justify-center items-center mx-auto">
-				{#each box as compartment, i}
-					<div class={`w-20 ${i === current ? 'bg-yellow-400/25' : ''}`}>
-						<div class="border border-solid border-black p-2 text-sm">
-							Sector {i + 1}
-							<p class="text-xs">{compartment.contents.length} of {compartment.capacity}</p>
-						</div>
-						<div class="border border-solid border-black h-48 flex flex-wrap">
-							{#each compartment.contents as med}
-								<div class={`${med.color} rounded-md h-2 w-2`}></div>
-							{/each}
-						</div>
-						<div class="flex flex-col gap-2 border border-black p-1">
-							{#each meds as med, j}
-								<button
-									class={`${med.color} text-xs rounded-md px-2 py-1 transition-all hover:scale-105`}
-									on:click={() => increment(i, j)}>+ {med.name}</button
-								>
-							{/each}
-						</div>
+		<div class="flex justify-center items-center mx-auto">
+			{#each box as compartment, i}
+				<div class={`w-20 ${i === current ? 'bg-yellow-400/25' : ''}`}>
+					<div class="border border-solid border-black p-2 text-sm">
+						Sector {i + 1}
+						<p class="text-xs">{compartment.contents.length} of {compartment.capacity}</p>
 					</div>
-				{/each}
-			</div>
-			<div class="flex flex-col gap-5 items-center">
-				<p>
-					It's time for you to take{' '}<span class="font-bold">Sector {current + 1}</span>&apos;s
-					meds!
-				</p>
-				<button
-					class="transition-all hover:scale-105 bg-blue-200 rounded-lg px-3 py-1"
-					on:click={() => clear(current)}>Open hatch</button
-				>
-			</div>
+					<div class="border border-solid border-black h-48 flex flex-wrap">
+						{#each compartment.contents as med}
+							<div class={`${med.color} rounded-md h-2 w-2`}></div>
+						{/each}
+					</div>
+					<div class="flex flex-col gap-2 border border-black p-1">
+						{#each meds as med, j}
+							<button
+								class={`${med.color} text-xs rounded-md px-2 py-1 transition-all hover:scale-105`}
+								on:click={() => increment(i, j)}>+ {med.name}</button
+							>
+						{/each}
+					</div>
+				</div>
+			{/each}
+		</div>
+		<div class="flex flex-col gap-2 items-center">
+			<p>
+				It's time for you to take{' '}<span class="font-bold">Sector {current + 1}</span>&apos;s
+				meds!
+			</p>
+			<button
+				class="transition-all hover:scale-105 bg-blue-200 rounded-lg px-3 py-1"
+				on:click={() => clear(current)}>Open hatch</button
+			>
 		</div>
 	</div>
 </div>
