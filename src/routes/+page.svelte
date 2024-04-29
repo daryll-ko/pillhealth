9ee60134afb8ab170e49a0161ce73af47b8e1442
Daryll Ko <daryll.ko@gmail.com>
2024-04-29 20:50:22 +0800

    update: make layout more similar to current proposal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,77 @@
 <script lang="ts">
+	interface Compartment {
+		contents: Med[];
+		capacity: number;
+	}
 	interface Med {
 		name: string;
 		color: string;
-		stock: number;
-		tray: number;
 	}
 	let meds: Med[] = [
 		{
 			name: 'med 1',
-			color: 'bg-red-300',
-			stock: 20,
-			tray: 0
+			color: 'bg-red-300'
 		},
 		{
 			name: 'med 2',
-			color: 'bg-blue-400',
-			stock: 50,
-			tray: 0
+			color: 'bg-blue-400'
 		},
 		{
 			name: 'med 3',
-			color: 'bg-green-400',
-			stock: 60,
-			tray: 0
+			color: 'bg-green-400'
 		},
 		{
 			name: 'med 4',
-			color: 'bg-orange-400',
-			stock: 5,
-			tray: 0
+			color: 'bg-orange-400'
 		}
 	];
-	function increment(name: string) {
-		meds = meds.map((med) =>
-			med.name === name ? { ...med, stock: Math.min(med.stock + 1, 100) } : med
+	let box: Compartment[] = [
+		{
+			contents: Array(1).fill(meds[0]),
+			capacity: 10
+		},
+		{
+			contents: Array(4).fill(meds[1]),
+			capacity: 10
+		},
+		{
+			contents: Array(5).fill(meds[2]),
+			capacity: 10
+		},
+		{
+			contents: Array(1).fill(meds[3]),
+			capacity: 10
+		},
+		{
+			contents: Array(4).fill(meds[0]),
+			capacity: 10
+		},
+		{
+			contents: Array(5).fill(meds[1]),
+			capacity: 10
+		},
+		{
+			contents: Array(1).fill(meds[2]),
+			capacity: 10
+		},
+		{
+			contents: Array(4).fill(meds[3]),
+			capacity: 10
+		}
+	];
+	// (i, j) = (compartment number, med number)
+	function increment(i: number, j: number) {
+		box = box.map((compartment, ii) =>
+			i === ii && compartment.contents.length + 1 <= compartment.capacity
+				? { ...compartment, contents: [...compartment.contents, meds[j]] }
+				: compartment
 		);
 	}
-	function decrement(name: string) {
-		meds = meds.map((med) =>
-			med.name === name ? { ...med, stock: Math.max(med.stock - 1, 0), tray: med.tray + 1 } : med
-		);
+	let current = 0;
+	setInterval(() => (current = (current + 1) % box.length), 1000);
+
+	function clear(i: number) {
+		box = box.map((compartment, ii) => (i === ii ? { ...compartment, contents: [] } : compartment));
 	}
 </script>
 
@@ -50,40 +82,39 @@
 			<h2 class="text-xl text-yellow-500">Your smart medicine box.</h2>
 		</div>
 
-		<div class="grid grid-rows-4 justify-center">
-			<div class="row-span-3 flex justify-center items-center mx-auto">
-				{#each meds as med}
-					<div class="w-20">
-						<div class="border border-solid border-black p-2">
-							{med.name}
-							<p class="text-xs">{med.stock} of 100</p>
+		<div class="flex items-center gap-5">
+			<div class="flex justify-center items-center mx-auto">
+				{#each box as compartment, i}
+					<div class={`w-20 ${i === current ? 'bg-yellow-400/25' : ''}`}>
+						<div class="border border-solid border-black p-2 text-sm">
+							Sector {i + 1}
+							<p class="text-xs">{compartment.contents.length} of {compartment.capacity}</p>
 						</div>
 						<div class="border border-solid border-black h-48 flex flex-wrap">
-							{#each { length: med.stock } as _, i}
+							{#each compartment.contents as med}
 								<div class={`${med.color} rounded-md h-2 w-2`}></div>
 							{/each}
 						</div>
-						<div class="border border-solid border-black flex flex-col">
-							<button
-								class="m-1 text-sm bg-green-300 rounded-lg px-1"
-								on:click={() => increment(med.name)}>refill</button
-							>
-							<button
-								disabled={med.stock === 0}
-								class="m-1 text-sm bg-yellow-400 rounded-lg px-1 disabled:text-black/20"
-								on:click={() => decrement(med.name)}>dispense</button
-							>
+						<div class="flex flex-col gap-2 border border-black p-1">
+							{#each meds as med, j}
+								<button
+									class={`${med.color} text-xs rounded-md px-2 py-1 transition-all hover:scale-105`}
+									on:click={() => increment(i, j)}>+ {med.name}</button
+								>
+							{/each}
 						</div>
 					</div>
 				{/each}
 			</div>
-
-			<div
-				class="row-span-1 border border-solid border-black flex flex-col justify-center items-center"
-			>
-				{#each meds as med}
-					<p class="text-sm">{med.name}: {med.tray}</p>
-				{/each}
+			<div class="flex flex-col gap-5 items-center">
+				<p>
+					It's time for you to take{' '}<span class="font-bold">Sector {current + 1}</span>&apos;s
+					meds!
+				</p>
+				<button
+					class="transition-all hover:scale-105 bg-blue-200 rounded-lg px-3 py-1"
+					on:click={() => clear(current)}>Open hatch</button
+				>
 			</div>
 		</div>
 	</div>
