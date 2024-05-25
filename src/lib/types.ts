4237ccb35f2e863f974d3bd3968ba6c1a568ad6f
Daryll Ko <daryll.ko@gmail.com>
2024-05-26 00:05:44 +0800

    update: tweak types a bit :wrench:

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,7 +4,7 @@ export interface Log {
 	medicine_name: string;
 	medicine_description: string;
 	sector: number;
-	user_id: number;
+	user_id: string;
 }
 
 export interface Medicine {
@@ -12,21 +12,21 @@ export interface Medicine {
 	name: string;
 	color: string;
 	description: string;
-	user_id: number;
-	in_sectors: Set<number>;
+	user_id: string;
+	in_sectors: number[];
 }
 
 export interface User {
-	user_id: number;
+	user_id: string;
 	email: string;
-	sector_1: Date;
-	sector_2: Date;
-	sector_3: Date;
-	sector_4: Date;
-	sector_5: Date;
-	sector_6: Date;
-	sector_7: Date;
-	sector_8: Date;
+	sector_1?: Date;
+	sector_2?: Date;
+	sector_3?: Date;
+	sector_4?: Date;
+	sector_5?: Date;
+	sector_6?: Date;
+	sector_7?: Date;
+	sector_8?: Date;
 }
 
 export interface Database {
