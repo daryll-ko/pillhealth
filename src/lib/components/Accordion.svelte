ed8ab21d3f9056385a8e2bc6221c5ff2fe9fdf50
Daryll Ko <daryll.ko@gmail.com>
2024-05-26 00:08:35 +0800

    update: upgrade `Accordion` for desc :test_tube:

--- src/lib/components/Accordion.svelte
+++ src/lib/components/Accordion.svelte
@@ -1,8 +1,13 @@
 <script>
-	export let open = false;
-	import { AngleDownOutline, AngleUpOutline, CirclePlusSolid } from 'flowbite-svelte-icons';
+	export let toggled = false;
+	export let editing = false;
+	export let deleting = false;
+
+	$: open = toggled || editing;
+
+	import { AngleDownOutline, AngleUpOutline } from 'flowbite-svelte-icons';
 	import { slide } from 'svelte/transition';
-	const handleClick = () => (open = !open);
+	const handleClick = () => (toggled = !toggled);
 </script>
 
 <div>
@@ -11,26 +16,33 @@
 			<slot name="head"></slot>
 		</div>
 
-		<button
-			on:click={handleClick}
-			class="py-1 group transition-all hover:scale-105 flex flex-row justify-center items-center gap-1 w-full"
-		>
-			<span class="text-theme/70 group-hover:text-theme/90">
-				{#if open}
-					<AngleUpOutline />
-				{:else}
-					<AngleDownOutline />
-				{/if}
-			</span>
-			<span>Description</span>
-			<span class="text-theme/70 group-hover:text-theme/90">
-				{#if open}
-					<AngleUpOutline />
-				{:else}
-					<AngleDownOutline />
-				{/if}
-			</span>
-		</button>
+		{#if deleting}
+			<p class="py-1 flex flex-row justify-center items-center gap-1 w-full">
+				Remove this medicine?
+			</p>
+		{:else}
+			<button
+				disabled={editing}
+				on:click={handleClick}
+				class="py-1 group transition-all hover:scale-105 flex flex-row justify-center items-center gap-1 w-full disabled:hover:scale-100"
+			>
+				<span class="text-theme/70 group-hover:text-theme/90">
+					{#if open}
+						<AngleUpOutline />
+					{:else}
+						<AngleDownOutline />
+					{/if}
+				</span>
+				<span>Description</span>
+				<span class="text-theme/70 group-hover:text-theme/90">
+					{#if open}
+						<AngleUpOutline />
+					{:else}
+						<AngleDownOutline />
+					{/if}
+				</span>
+			</button>
+		{/if}
 	</div>
 
 	{#if open}
