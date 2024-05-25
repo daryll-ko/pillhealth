c01bda674d5fc237f9b76f4e56a6eb6b604cdc16
Daryll Ko <daryll.ko@gmail.com>
2024-05-26 00:07:50 +0800

    add: include initial `History` page :memo:

---
+++ src/routes/private/history/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+		Checkbox,
+		TableSearch
+	} from 'flowbite-svelte';
+
+	export let data;
+
+	$: ({ logs } = data);
+
+	$: logsToShow = logs.sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime());
+</script>
+
+<div class="flex flex-col w-[80%] gap-12 p-12">
+	<h1 class="text-4xl font-bold">History</h1>
+	<Table hoverable={true} divClass="bg-black/10 rounded-md">
+		<TableHead theadClass="">
+			<TableHeadCell>Date</TableHeadCell>
+			<TableHeadCell>Type</TableHeadCell>
+			<TableHeadCell>Medicine Name</TableHeadCell>
+			<TableHeadCell>Sector</TableHeadCell>
+		</TableHead>
+		<TableBody tableBodyClass="divide-y text-center">
+			{#each logsToShow as log}
+				<TableBodyRow>
+					<TableBodyCell tdClass="py-4"
+						><span class="text-black">{new Date(log.timestamp).toLocaleString()}</span
+						></TableBodyCell
+					>
+					<TableBodyCell><span class="text-black">{log.type}</span></TableBodyCell>
+					<TableBodyCell><span class="text-black">{log.medicine_name}</span></TableBodyCell>
+					<TableBodyCell><span class="text-black">{log.sector + 1}</span></TableBodyCell>
+				</TableBodyRow>
+			{/each}
+		</TableBody>
+	</Table>
+</div>
