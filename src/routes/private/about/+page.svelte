0c4395d20bbfe271cb9a4979417670fbd6a5aded
Daryll Ko <daryll.ko@gmail.com>
2024-05-29 17:17:13 +0800

    update: UI polishing

--- src/routes/private/about/+page.svelte
+++ src/routes/private/about/+page.svelte
@@ -8,8 +8,8 @@
 	<div class="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
 		<div class="flex flex-col lg:flex-row justify-between gap-8">
 				<div class="w-full lg:w-5/12 flex flex-col justify-center">
-					<h1 class="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 dark:text-white pb-4">About Us</h1>
-					<p class="font-normal text-base leading-6 text-gray-600 dark:text-white text-justify">
+					<h1 class="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">About Us</h1>
+					<p class="font-normal text-base leading-6 text-gray-600 text-justify">
                                                 PillHealth is an automated medicine box that aims to revolutionize
                                                 medication management. Say goodbye to missed doses and confusion -
                                                 PillHealth keeps you on track with timely visual and audio reminders and
@@ -30,8 +30,8 @@
 
 		<div class="flex lg:flex-row flex-col justify-between gap-8 pt-12">
 			<div class="w-full lg:w-5/12 flex flex-col justify-center">
-				<h1 class="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 dark:text-white pb-4">Vision & Mission</h1>
-				<p class="font-normal text-base leading-6 text-gray-600 dark:text-white text-justify">
+				<h1 class="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Vision & Mission</h1>
+				<p class="font-normal text-base leading-6 text-gray-600 text-justify">
                                         PillHealth was founded in 2024 by a group of passionate Computer Science students
                                         from the University of the Philippines - Diliman. Our team is dedicated to improving the
                                         lives of individuals with chronic conditions by providing a simple and
@@ -49,29 +49,29 @@
                 <div class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-4  rounded-md">
                     <div class="p-4 pb-6 flex justify-center flex-col items-center">
                                     <img class="md:block hidden" src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png" alt="Alexa featured Image" />
-                                    <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Pauline</p>
+                                    <p class="font-medium text-xl leading-5 text-gray-800 g mt-4">Pauline</p>
                     </div>
                     <div class="p-4 pb-6 flex justify-center flex-col items-center">
                                     <img class="md:block hidden" src="https://i.ibb.co/fGmxhVy/Rectangle-119.png" alt="Olivia featured Image" />
-                                    <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Immanuel</p>
+                                    <p class="font-medium text-xl leading-5 text-gray-800 g mt-4">Immanuel</p>
                     </div>
                     <div class="p-4 pb-6 flex justify-center flex-col items-center">
                                     <img class="md:block hidden" src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png" alt="Liam featued Image" />
-                                    <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Daryll</p>
+                                    <p class="font-medium text-xl leading-5 text-gray-800 g mt-4">Daryll</p>
                     </div>
                 </div>
                 <div class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
                     <div class="p-4 pb-6 flex justify-center flex-col items-center">
                             <img class="md:block hidden" src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png" alt="Alexa featured Image" />
-                            <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Westin</p>
+                            <p class="font-medium text-xl leading-5 text-gray-800 g mt-4">Westin</p>
                     </div>
                     <div class="p-4 pb-6 flex justify-center flex-col items-center">
                             <img class="md:block hidden" src="https://i.ibb.co/fGmxhVy/Rectangle-119.png" alt="Olivia featured Image" />
-                            <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Rohan</p>
+                            <p class="font-medium text-xl leading-5 text-gray-800 g mt-4">Rohan</p>
                     </div>
                     <div class="p-4 pb-6 flex justify-center flex-col items-center">
                             <img class="md:block hidden" src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png" alt="Liam featued Image" />
-                            <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Jela</p>
+                            <p class="font-medium text-xl leading-5 text-gray-800 g mt-4">Jela</p>
                     </div>
                 </div>
 			</div>
--- src/routes/private/faq/+page.svelte
+++ src/routes/private/faq/+page.svelte
@@ -1,75 +1,112 @@
 <script lang="ts">
-  import { ExpansionPanel } from 'svelte-mui';
+	import { ExpansionPanel } from 'svelte-mui';
 	let group = '';
 
 	const onchage = ({ detail }) => {
-	// console.log(detail.expanded ? 'open' : 'close', detail.name);
-  };
+		// console.log(detail.expanded ? 'open' : 'close', detail.name);
+	};
 </script>
 
-<div class="flex flex-row h-screen bg-[url('/src/lib/assets/FAQ.png')] bg-right bg-no-repeat bg scaled-bg mb-10"> 
+<div
+	class="flex flex-row h-screen bg-[url('/src/lib/assets/FAQ.png')] bg-right bg-no-repeat bg scaled-bg mb-10"
+>
 	<div class="flex flex-col w-[80%] gap-8">
 		<div class="text-[#333333] text-left flex flex-col gap-3 ml-20 w-[70%]">
 			<h1 class="text-4xl font-bold">Frequently Asked Questions</h1>
 			<p class="text-2xl mb-10">Learn how PillHealth can treat you better.</p>
-      	<div>
-			<ExpansionPanel 
-			name="**How do I schedule a medicine intake?" bind:group on:change={onchage} --focus-color="#000000">
-				<div>
-					On the dashboard, click the “+” icon on the right side of the medication bar to add a medicine. 
-					A pop up will show where you add its details such as the name, description, and frequency.
-					Thereafter selecting a compartment will allow After clicking “Add,” it should show up on the medication bar
-				</div>
-			</ExpansionPanel>
-			<ExpansionPanel name="How do I refill the compartments?" bind:group on:change={onchage}>
+			<div>
+				<ExpansionPanel
+					name="How do I add a medicine?"
+					bind:group
+					on:change={onchage}
+					--focus-color="#000000"
+				>
 					<div>
-						To refill the compartments, open the device and load the pills into the designated compartments you have indicated on the app according to your medication schedule.
+						Click the 'See all Medications' button. On the side bar click '+ Add Medication' and input your desired medicine name and description in the popup and hit 'Save'. Selecting a sector and clicking '+ Add Medication' will now allow you to select one of your medications to add to the sector.
 					</div>
 				</ExpansionPanel>
-			<!-- <ExpansionPanel name="What happens if I miss a dose?" bind:group on:change={onchage}>
+				<ExpansionPanel
+					name="How do I schedule a medicine intake?"
+					bind:group
+					on:change={onchage}
+					--focus-color="#000000"
+				>
+					<div>
+						Select the sector you want to set a schedule for, then simply input the desired date and
+						time you wish to schedule your medicine intake in the right sidebar and click 'Save
+						time'.
+					</div>
+				</ExpansionPanel>
+				<ExpansionPanel name="How do I refill the compartments?" bind:group on:change={onchage}>
+					<div>
+						To refill the compartments, open the device and load the pills into the designated
+						compartments you have indicated on the app according to your medication schedule.
+					</div>
+				</ExpansionPanel>
+				<!-- <ExpansionPanel name="What happens if I miss a dose?" bind:group on:change={onchage}>
 					<div>
 						If you miss a dose, PillHealth will send repeated reminders until you take your medication. The device also logs missed doses, which you can review through the app. This information can be shared with your healthcare provider if needed.
 					</div>
 				</ExpansionPanel> -->
-			<ExpansionPanel name="What do I place in the description?" bind:group on:change={onchage}>
+				<ExpansionPanel name="What do I place in the description?" bind:group on:change={onchage}>
 					<div>
-						You may customize the description according to your liking to serve as a reminder.
-						It can be the purpose of the medication, the dosage, or any other information you want to include.
+						You may customize the description according to your liking to serve as a reminder. It
+						can be the purpose of the medication, the dosage, or any other information you want to
+						include.
 					</div>
 				</ExpansionPanel>
-			<ExpansionPanel name="Can PillHealth dispense multiple medications?" bind:group on:change={onchage}>
+				<ExpansionPanel
+					name="Can PillHealth dispense multiple medications?"
+					bind:group
+					on:change={onchage}
+				>
 					<div>
-						Yes, PillHealth is equipped with multiple compartments that can hold different types of medications. You can program each compartment to dispense at specific times according to your prescribed schedule.
+						Yes, PillHealth is equipped with multiple compartments that can hold different types of
+						medications. You can program each compartment to dispense at specific times according to
+						your prescribed schedule.
 					</div>
 				</ExpansionPanel>
-			<ExpansionPanel name="What types of reminders does PillHealth provide?" bind:group on:change={onchage}>
+				<ExpansionPanel
+					name="What types of reminders does PillHealth provide?"
+					bind:group
+					on:change={onchage}
+				>
 					<div>
-						PillHealth provides audible and visual alarms from the device along with email reminders.
+						PillHealth provides audible and visual alarms from the device along with email
+						reminders.
 					</div>
 				</ExpansionPanel>
-			<ExpansionPanel name="What if there is a power outage?" bind:group on:change={onchage}>
+				<ExpansionPanel name="What if there is a power outage?" bind:group on:change={onchage}>
 					<div>
-						PillHealth has a built-in battery backup that ensures it continues to operate during a power outage. Once power is restored, the device will recharge automatically.
+						PillHealth has a built-in battery backup that ensures it continues to operate during a
+						power outage. Once power is restored, the device will recharge automatically.
 					</div>
 				</ExpansionPanel>
-			<ExpansionPanel name="How do I clean and maintain PillHealth?" bind:group on:change={onchage}>
+				<ExpansionPanel
+					name="How do I clean and maintain PillHealth?"
+					bind:group
+					on:change={onchage}
+				>
 					<div>
-						To clean PillHealth, use a soft, damp cloth to wipe the exterior. Do not use harsh chemicals or submerge the device in water. Regularly check the compartments for any pill residue and clean them as needed..
+						To clean PillHealth, use a soft, damp cloth to wipe the exterior. Do not use harsh
+						chemicals or submerge the device in water. Regularly check the compartments for any pill
+						residue and clean them as needed..
 					</div>
 				</ExpansionPanel>
-			<ExpansionPanel name="What if I need technical support?" bind:group on:change={onchage}>
+				<ExpansionPanel name="What if I need technical support?" bind:group on:change={onchage}>
 					<div>
-						Feel free to email us at pillhealth145@gmail.com and we will get back to you as soon as possible to provide assistance.
+						Feel free to email us at pillhealth145@gmail.com and we will get back to you as soon as
+						possible to provide assistance.
 					</div>
-			</ExpansionPanel>
-		</div>
+				</ExpansionPanel>
+			</div>
 		</div>
 	</div>
 </div>
 
 <style>
-    .scaled-bg {
-      background-size: 90%; /* Adjust the percentage to scale the background image */
-	  background-position-y: 10%;
-    }
-  </style>
+	.scaled-bg {
+		background-size: 90%; /* Adjust the percentage to scale the background image */
+		background-position-y: 10%;
+	}
+</style>
